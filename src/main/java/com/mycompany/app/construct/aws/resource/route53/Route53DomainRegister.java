7235480ef8151e0db1ef6d258b085cc0f1901e60
K-Diger <kitten.diger@gmail.com>
2023-04-05 12:12:05 +0900

    feat : Route53 환경변수를 사용하여 도메인 등록 요청을 보내는 기능을 추가했습니다.

--- src/main/java/com/mycompany/app/construct/aws/resource/route53/Route53DomainRegister.java
+++ src/main/java/com/mycompany/app/construct/aws/resource/route53/Route53DomainRegister.java
@@ -1,53 +1,57 @@
 package com.mycompany.app.construct.aws.resource.route53;
 
+import static com.mycompany.app.constant.Configuration.Aws.NAME_SERVER_NAME1;
+import static com.mycompany.app.constant.Configuration.Aws.NAME_SERVER_NAME2;
+import static com.mycompany.app.constant.Configuration.Aws.NAME_SERVER_NAME3;
+import static com.mycompany.app.constant.Configuration.Aws.NAME_SERVER_NAME4;
+
 import com.amazonaws.services.route53domains.AmazonRoute53Domains;
 import com.amazonaws.services.route53domains.AmazonRoute53DomainsClientBuilder;
 import com.amazonaws.services.route53domains.model.ContactDetail;
-import com.amazonaws.services.route53domains.model.ContactType;
 import com.amazonaws.services.route53domains.model.DomainLimitExceededException;
 import com.amazonaws.services.route53domains.model.InvalidInputException;
-import com.amazonaws.services.route53domains.model.Nameserver;
 import com.amazonaws.services.route53domains.model.RegisterDomainRequest;
-import com.amazonaws.services.route53domains.model.RegisterDomainResult;
+import com.mycompany.app.constant.Configuration.Aws;
+import java.util.List;
+
+/**
+ * 도메인 등록 요청을 위한 필수 필드 Argument: domain_name - (Required) The name of the registered domain. The
+ * name_server Name_Server object supports the following: name - (Required) The fully qualified host
+ * name of the name server.
+ */
 
 public class Route53DomainRegister {
 
-    public static void main(String[] args) {
+    // 등록할 도메인 이름
+    private final String DOMAIN_NAME = Aws.DOMAIN_NAME;
+    // 네임서버 이름
+    private final List<String> NAME_SERVER_NAME = List.of(
+        NAME_SERVER_NAME1,
+        NAME_SERVER_NAME2,
+        NAME_SERVER_NAME3,
+        NAME_SERVER_NAME4
+    );
+    private final Integer DOMAIN_DURATION = Aws.DOMAIN_DURATION;
 
-        String domainName = "example.com"; // 등록할 도메인 이름
-        String adminContactFirstName = "John"; // 도메인 관리자의 이름
-        String adminContactLastName = "Doe"; // 도메인 관리자의 성
-        String adminContactEmail = "john.doe@example.com"; // 도메인 관리자의 이메일
-        String adminContactPhoneNumber = "+1.5555555555"; // 도메인 관리자의 전화번호
-        String adminContactAddressLine1 = "123 Main St"; // 도메인 관리자의 주소
-        String adminContactCity = "Anytown"; // 도메인 관리자의 도시
-        String adminContactState = "NY"; // 도메인 관리자의 주
-        String adminContactCountryCode = "US"; // 도메인 관리자의 국가 코드
-        String adminContactZipCode = "12345"; // 도메인 관리자의 우편번호
+    public Route53DomainRegister() {
 
+        // 클라이언트 받아오기
         AmazonRoute53Domains client = AmazonRoute53DomainsClientBuilder.standard().build();
-        ContactDetail adminContactDetail = new ContactDetail()
-            .withFirstName(adminContactFirstName)
-            .withLastName(adminContactLastName)
-            .withEmail(adminContactEmail)
-            .withPhoneNumber(adminContactPhoneNumber)
-            .withAddressLine1(adminContactAddressLine1)
-            .withCity(adminContactCity)
-            .withState(adminContactState)
-            .withCountryCode(adminContactCountryCode)
-            .withZipCode(adminContactZipCode)
-            .withContactType(ContactType.ADMINISTRATIVE);
 
+        // 도메인 등록자의 정보
+        ContactDetail adminContactDetail = new ContactDetail();
+
+        // 도메인 등록 요청 객체 생성
         RegisterDomainRequest request = new RegisterDomainRequest()
-            .withDomainName(domainName)
-            .withDurationInYears(1)
+            .withDomainName(DOMAIN_NAME)
+            .withDurationInYears(DOMAIN_DURATION)
             .withAdminContact(adminContactDetail);
 
         try {
-            RegisterDomainResult result = client.registerDomain(request);
-            System.out.println("Registered domain: " + result.getDomainName());
-            for (Nameserver nameserver : result.getNameservers()) {
-                System.out.println("Nameserver: " + nameserver.getName());
+            // 도메인 등록 요청을 보낸다.
+            client.registerDomain(request);
+            for (String nameServerName : NAME_SERVER_NAME) {
+                System.out.println("nameServerName = " + nameServerName);
             }
         } catch (DomainLimitExceededException e) {
             System.err.println("Domain limit exceeded: " + e.getMessage());
@@ -55,5 +59,4 @@ public class Route53DomainRegister {
             System.err.println("Invalid input: " + e.getMessage());
         }
     }
-}
 }
