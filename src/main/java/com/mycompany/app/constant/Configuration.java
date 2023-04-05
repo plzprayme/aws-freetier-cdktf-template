a5df89ff521f868f84a4c22a829f549761d4652e
K-Diger <kitten.diger@gmail.com>
2023-04-05 12:11:29 +0900

    feat : Route53 환경변수 Configuration 추가하였습니다.

--- src/main/java/com/mycompany/app/constant/Configuration.java
+++ src/main/java/com/mycompany/app/constant/Configuration.java
@@ -7,12 +7,21 @@ public class Configuration {
     private static Map<String, String> ENV = System.getenv();
 
     public static class Aws {
+
         public static String AWS_ACCESS_KEY = ENV.get("AWS_ACCESS_KEY");
         public static String AWS_SECRET_KEY = ENV.get("AWS_SECRET_KEY");
         public static String REGION = ENV.get("REGION");
+
+        public static String DOMAIN_NAME = ENV.get("DOMAIN_NAME");
+        public static String NAME_SERVER_NAME1 = ENV.get("NAME_SERVER_NAME1");
+        public static String NAME_SERVER_NAME2 = ENV.get("NAME_SERVER_NAME2");
+        public static String NAME_SERVER_NAME3 = ENV.get("NAME_SERVER_NAME3");
+        public static String NAME_SERVER_NAME4 = ENV.get("NAME_SERVER_NAME4");
+        public static Integer DOMAIN_DURATION = Integer.valueOf(ENV.get("DOMAIN_DURATION"));
     }
 
     public static class TFCloud {
+
         public static String ORGANIZATION = ENV.get("TF_CLOUD_ORGANIZATION");
         public static String WORKSPACE = ENV.get("TF_WORKSPACE");
         public static String TOKEN = ENV.get("TF_TOKEN");
