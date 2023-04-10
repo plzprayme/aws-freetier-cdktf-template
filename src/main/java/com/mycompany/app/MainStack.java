d3332b41cecfdb659e01c1a5547df40c885fa507
K-Diger <kitten.diger@gmail.com>
2023-04-11 00:11:26 +0900

    style(MainStack): 코드 정렬

--- src/main/java/com/mycompany/app/MainStack.java
+++ src/main/java/com/mycompany/app/MainStack.java
@@ -1,5 +1,6 @@
 package com.mycompany.app;
 
+import com.hashicorp.cdktf.TerraformStack;
 import com.hashicorp.cdktf.providers.aws.cloudwatch_log_group.CloudwatchLogGroup;
 import com.hashicorp.cdktf.providers.aws.cloudwatch_log_stream.CloudwatchLogStream;
 import com.hashicorp.cdktf.providers.aws.elastic_beanstalk_application.ElasticBeanstalkApplication;
@@ -16,18 +17,19 @@ import com.mycompany.app.construct.aws.iam.profile.EBIamInstanceProfile;
 import com.mycompany.app.construct.aws.iam.role.EBIamRole;
 import com.mycompany.app.construct.aws.provider.AwsProvider;
 import com.mycompany.app.construct.aws.resource.cloud_watch.CWLogGroup;
-import com.mycompany.app.construct.aws.resource.eb.*;
+import com.mycompany.app.construct.aws.resource.eb.EBApp;
+import com.mycompany.app.construct.aws.resource.eb.EBAppVer;
+import com.mycompany.app.construct.aws.resource.eb.EBEnv;
+import com.mycompany.app.construct.aws.resource.eb.EBEnvSetting;
 import com.mycompany.app.construct.aws.resource.route53.Route53DomainRegister;
 import com.mycompany.app.construct.aws.resource.s3.SourceBundleS3Bucket;
 import com.mycompany.app.construct.aws.resource.s3.SourceBundleS3Object;
-import software.constructs.Construct;
-
-import com.hashicorp.cdktf.TerraformStack;
-
 import java.util.List;
+import software.constructs.Construct;
 
 // aws provider argument docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs#argument-reference
 public class MainStack extends TerraformStack {
+
     public MainStack(final Construct scope, final String id) {
         super(scope, id);
 
@@ -37,14 +39,14 @@ public class MainStack extends TerraformStack {
         CloudwatchLogGroup logGroup = provisionLogGroup(this);
         provisionLogStream(this, logGroup);
 
-
 //        https://blog.hbsmith.io/aws-elastic-beanstalk-amazon-linux2-%ED%99%98%EA%B2%BD%EC%97%90%EC%84%9C%EC%9D%98-%EB%A1%9C%EA%B7%B8-%EC%BB%A4%EC%8A%A4%ED%84%B0%EB%A7%88%EC%9D%B4%EC%A7%95-amazon-cloudwatch-eb-web-console-1dcb9cc79316
 //        https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/ebextensions.html
     }
 
     private void provisionProvider(Construct scope) {
-        new AwsProvider(Configuration.Aws.REGION, Configuration.Aws.AWS_ACCESS_KEY, Configuration.Aws.AWS_SECRET_KEY)
-                .provision(scope);
+        new AwsProvider(Configuration.Aws.REGION, Configuration.Aws.AWS_ACCESS_KEY,
+            Configuration.Aws.AWS_SECRET_KEY)
+            .provision(scope);
     }
 
     private ElasticBeanstalkApplication provisionElasticBeanstalk(Construct scope) {
@@ -52,16 +54,18 @@ public class MainStack extends TerraformStack {
 
         S3Bucket s3Bucket = new SourceBundleS3Bucket().provision(scope);
         S3Object s3Object = new SourceBundleS3Object(s3Bucket).provision(scope);
-        ElasticBeanstalkApplicationVersion ebAppVer = new EBAppVer(ebApp, s3Bucket, s3Object).provision(scope);
+        ElasticBeanstalkApplicationVersion ebAppVer = new EBAppVer(ebApp, s3Bucket,
+            s3Object).provision(scope);
 
         IamRole role = new EBIamRole().provision(scope);
         IamInstanceProfile profile = new EBIamInstanceProfile(role).provision(scope);
-        List<ElasticBeanstalkEnvironmentSetting> settings = new EBEnvSetting(profile).provision(scope);
+        List<ElasticBeanstalkEnvironmentSetting> settings = new EBEnvSetting(profile).provision(
+            scope);
         ElasticBeanstalkEnvironment ebEnv = new EBEnv(ebApp, ebAppVer, settings).provision(scope);
 
         // Route53 서비스 추가
         Route53DomainsRegisteredDomain route53 = new Route53DomainRegister().provision(scope);
-        
+
         return ebApp;
     }
 
