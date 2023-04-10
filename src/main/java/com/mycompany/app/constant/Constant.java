ea0769d44a77e194ba62f46b7a069e4d245dec3c
K-Diger <kitten.diger@gmail.com>
2023-04-11 00:09:26 +0900

    fix(Constant): COMMIT_SHA -> COMMIT_HASH 필드명 변경 및 줄바꿈 추가

--- src/main/java/com/mycompany/app/constant/Constant.java
+++ src/main/java/com/mycompany/app/constant/Constant.java
@@ -8,21 +8,26 @@ public class Constant {
     private static Map<String, String> ENV = System.getenv();
 
     public static class Version {
+
         public static String PROJECT_NAME = ENV.get("PROJECT_NAME");
-        public static String COMMIT_SHA = ENV.get("COMMIT_SHA");
+        public static String COMMIT_HASH = ENV.get("COMMIT_HASH");
         public static LocalDateTime TIMESTAMP = LocalDateTime.now();
 
         public static String VERSION =
-                String.join("-", new String[] {PROJECT_NAME, COMMIT_SHA.substring(0, 5), TIMESTAMP.toString()});
+            String.join("-", new String[]{
+                PROJECT_NAME, COMMIT_HASH.substring(0, 5), TIMESTAMP.toString()
+            });
     }
 
     public static class Resource {
 
         public static class ElasticBeanstalk {
+
             public static String SOLUTION_STACK = ENV.get("EB_SOLUTION_STACK");
         }
 
         public static class S3 {
+
             public static String SOURCE_BUNDLE_PATH = ENV.get("SOURCE_BUNDLE_PATH");
         }
 
