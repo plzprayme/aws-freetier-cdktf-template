18b0af74ee1b06959844d4daf2ac1e7e1cba9e7e
plzprayme <dbfpzk142@gmail.com>
2023-04-11 01:20:57 +0900

    스프링 애플리케이션이 로그를 남기도록 변경

--- application/src/main/java/aws/cday/demo/Application.java
+++ application/src/main/java/aws/cday/demo/Application.java
@@ -1,13 +1,20 @@
 package aws.cday.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class Application {
+
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -24,6 +31,7 @@ public class Application {
 
 		@GetMapping("/hello")
 		public String hell() {
+			log.info("Hello World! | " + LocalDateTime.now());
 			return "Hello 2022 Community Day!!";
 		}
 	}
