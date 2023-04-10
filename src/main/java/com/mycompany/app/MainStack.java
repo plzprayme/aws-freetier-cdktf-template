36f4c875c006746e88c2f830df2413851592363c
jeongmin-han <jeongmin.han@navercorp.com>
2023-04-10 23:17:58 +0900

    add : rds, default vpc

--- src/main/java/com/mycompany/app/MainStack.java
+++ src/main/java/com/mycompany/app/MainStack.java
@@ -2,6 +2,7 @@ package com.mycompany.app;
 
 import com.hashicorp.cdktf.providers.aws.cloudwatch_log_group.CloudwatchLogGroup;
 import com.hashicorp.cdktf.providers.aws.cloudwatch_log_stream.CloudwatchLogStream;
+import com.hashicorp.cdktf.providers.aws.db_instance.DbInstance;
 import com.hashicorp.cdktf.providers.aws.elastic_beanstalk_application.ElasticBeanstalkApplication;
 import com.hashicorp.cdktf.providers.aws.elastic_beanstalk_application_version.ElasticBeanstalkApplicationVersion;
 import com.hashicorp.cdktf.providers.aws.elastic_beanstalk_environment.ElasticBeanstalkEnvironment;
@@ -15,9 +16,14 @@ import com.mycompany.app.construct.aws.iam.profile.EBIamInstanceProfile;
 import com.mycompany.app.construct.aws.iam.role.EBIamRole;
 import com.mycompany.app.construct.aws.provider.AwsProvider;
 import com.mycompany.app.construct.aws.resource.cloud_watch.CWLogGroup;
-import com.mycompany.app.construct.aws.resource.eb.*;
+import com.mycompany.app.construct.aws.resource.eb.EBApp;
+import com.mycompany.app.construct.aws.resource.eb.EBAppVer;
+import com.mycompany.app.construct.aws.resource.eb.EBEnv;
+import com.mycompany.app.construct.aws.resource.eb.EBEnvSetting;
+import com.mycompany.app.construct.aws.resource.rds.RDSInstance;
 import com.mycompany.app.construct.aws.resource.s3.SourceBundleS3Bucket;
 import com.mycompany.app.construct.aws.resource.s3.SourceBundleS3Object;
+import com.mycompany.app.construct.aws.resource.vpc.AwsDefaultVpc;
 import software.constructs.Construct;
 
 import com.hashicorp.cdktf.TerraformStack;
@@ -35,7 +41,7 @@ public class MainStack extends TerraformStack {
         CloudwatchLogGroup logGroup = provisionLogGroup(this);
         provisionLogStream(this, logGroup);
 
-
+        provisionRDS(this);
 //        https://blog.hbsmith.io/aws-elastic-beanstalk-amazon-linux2-%ED%99%98%EA%B2%BD%EC%97%90%EC%84%9C%EC%9D%98-%EB%A1%9C%EA%B7%B8-%EC%BB%A4%EC%8A%A4%ED%84%B0%EB%A7%88%EC%9D%B4%EC%A7%95-amazon-cloudwatch-eb-web-console-1dcb9cc79316
 //        https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/ebextensions.html
     }
@@ -56,7 +62,7 @@ public class MainStack extends TerraformStack {
         IamInstanceProfile profile = new EBIamInstanceProfile(role).provision(scope);
         List<ElasticBeanstalkEnvironmentSetting> settings = new EBEnvSetting(profile).provision(scope);
         ElasticBeanstalkEnvironment ebEnv = new EBEnv(ebApp, ebAppVer, settings).provision(scope);
-        
+
         return ebApp;
     }
 
@@ -66,5 +72,11 @@ public class MainStack extends TerraformStack {
 
     private CloudwatchLogStream provisionLogStream(Construct scope, CloudwatchLogGroup logGroup) {
         return new CWLogStream(logGroup).provision(scope);
+    }
+
+    private DbInstance provisionRDS(Construct scope) {
+        new AwsDefaultVpc().provision(scope);
+
+        return new RDSInstance().provision(scope);
     }
 }
--- src/main/java/com/mycompany/app/constant/Constant.java
+++ src/main/java/com/mycompany/app/constant/Constant.java
@@ -13,7 +13,7 @@ public class Constant {
         public static LocalDateTime TIMESTAMP = LocalDateTime.now();
 
         public static String VERSION =
-                String.join("-", new String[] {PROJECT_NAME, COMMIT_HASH.substring(0, 5), TIMESTAMP.toString()});
+                String.join("-", new String[]{PROJECT_NAME, COMMIT_HASH.substring(0, 5), TIMESTAMP.toString()});
     }
 
     public static class Resource {
@@ -26,5 +26,9 @@ public class Constant {
             public static String SOURCE_BUNDLE_PATH = ENV.get("SOURCE_BUNDLE_PATH");
         }
 
+        public static class RDS {
+            public static String RDS_USER_NAME = ENV.get("RDS_USER_NAME");
+            public static String RDS_PASSWORD = ENV.get("RDS_PASSWORD");
+        }
     }
 }
