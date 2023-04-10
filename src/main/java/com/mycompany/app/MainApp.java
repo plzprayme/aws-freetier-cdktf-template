3d0461741a278bb10350b273e6f2a44ccc887244
K-Diger <kitten.diger@gmail.com>
2023-04-11 00:10:26 +0900

    fix(Main): 기존 File을 통해 환경설정을 읽기 위한 코드 제거

--- src/main/java/com/mycompany/app/MainApp.java
+++ src/main/java/com/mycompany/app/MainApp.java
@@ -7,48 +7,14 @@ import com.hashicorp.cdktf.NamedCloudWorkspace;
 import com.hashicorp.cdktf.TerraformStack;
 import com.mycompany.app.constant.Configuration;
 import com.mycompany.app.constant.Constant;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-        File file = new File(".env.tmpl");
-        Map<String, String> env = new HashMap<>();
-
-        try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] parts = line.split("=", 2);
-                String key = parts[0].replaceAll("\"", "");
-                String value = parts[1].replaceAll("\"", "");
-                env.put(key, value);
-            }
-
-            bufferedReader.close();
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("region = " + env.get("REGION"));
-        System.out.println("COMMIT_HASH = " + env.get("COMMIT_HASH"));
-
-//        Map<String, String> ENV = System.getenv();
-//        for (Entry<String, String> stringStringEntry : ENV.entrySet()) {
-//            System.out.println("stringStringEntry = " + stringStringEntry);
-//        }
-
         final App app = new App();
+
         MainStack stack = new MainStack(app, Constant.Version.PROJECT_NAME + "-dev");
         provisionTFCloud(
             stack,
